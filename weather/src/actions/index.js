a5dec3e5eba23ed6b95ed84b17dd1603d79bcb55
Pablo Aravena <paravena@homeemergency.com>
2018-05-05 11:26:04 -0300

    first commit

---
+++ weather/src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = '6a045410972e0ae573d05af5a65fea84';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
