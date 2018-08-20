7128f53414e0fbadaf6de37d5f9bd6e8f2f5207b
Pablo Aravena <paravena@homeemergency.com>
2018-08-20 18:41:33 -0300

    server finished

--- testing/server/index.js
+++ testing/server/index.js
@@ -4,11 +4,16 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
-router(app);
+const mongoose = require('mongoose');
+
+// DB setup
+mongoose.connect('mongodb://localhost:27017/auth');
+
 
 // App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const port = process.env.PORT || 3090;
