8c5f00c8dfb5e6e1e3a4220db283f7de75e5cc30
Pablo Aravena <paravena@homeemergency.com>
2018-08-23 23:58:20 -0300

    adding auth app

---
+++ auth/client/src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
+import reducers from './reducers';
+import App from './components/App';
+import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
+import Feature from './components/Feature';
+
+const store = createStore(
+  reducers,
+  {
+    auth: { authenticated: localStorage.getItem('token') }
+  },
+  applyMiddleware(reduxThunk)
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/feature" exact component={Feature} />
+        <Route path="/signout" exact component={Signout} />
+        <Route path="/signin" exact component={Signin} />
+      </App>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('#root')
+)
